455a443aef6ab226f3b0f20c89a79559806093ed
Plover Brown <rellwoos@gmail.com>
2020-07-29 21:36:10 -0700

    edited tags, organized css file, added global variable notes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,9 @@
 'use strict'
+/*global moment, $, localStorage*/
+/*eslint no-undef: "error"*/
 
 //VARIABLES
 
-//PREKNOWN CONSTANTS
-
-
-//CHANGEABLE VARIABLES
 let workHour = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM"];
 let workHour24 = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
 let now = moment().format("dddd, MMMM D, YYYY");
@@ -63,7 +61,7 @@ function getLocalStorageOrDefault(key){
 function writeHours() {
     for (let hour of workHour) {
         //create blocks
-        const hourDiv = $(`<div id="${hour}-row" class="row time-block"><span id="${hour}-span" class="hour col-1">${hour}</span><textarea id="${hour}" class="col-10"></textarea><button id="${hour}-btn" value="${hour}" class="saveBtn btn col-1 icon-save"></button></div>`);
+        const hourDiv = $(`<div id="${hour}-row" class="row time-block"><div id="${hour}-div" class="hour col-1">${hour}</div><textarea id="${hour}" class="col-10"></textarea><button id="${hour}-btn" value="${hour}" class="saveBtn btn col-1 icon-save"></button></div>`);
 
         //retrieve and assign text
         const hourText = hourDiv.children(`#${hour}`);
